aaf4046f7374f1240a5a0f81e457bcf12e0ffe1b
wangshaojuan <wangshaojuan@allyamall.com>
2023-03-30 21:24:51 +0800

    feature: create project

---
+++ client/src/views/home/HomeTitle.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home-title">
+    <div class="home-title__info">
+      <VanImage
+        width="20"
+        height="20"
+        round
+        src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
+      />
+      <Button plain type="danger">测试</Button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { Image as VanImage, Button } from 'vant';
+</script>
---
+++ client/src/views/home/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home">
+    <HomeTitle />
+  </div>
+</template>
+
+<script setup>
+  import HomeTitle from './HomeTitle.vue';
+</script>
